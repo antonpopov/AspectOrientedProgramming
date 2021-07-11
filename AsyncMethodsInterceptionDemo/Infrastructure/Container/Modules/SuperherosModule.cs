21d8e7f3b848ca15bc72173a700da298299cb340
Anton Popov <anton.a.popov@gmail.com>
2021-07-11 12:32:31 +0300

    Registering Interceptors with Castle.Core.AsyncInterceptor in Autofac

--- AsyncMethodsInterceptionDemo/Infrastructure/Container/Modules/SuperherosModule.cs
+++ AsyncMethodsInterceptionDemo/Infrastructure/Container/Modules/SuperherosModule.cs
@@ -4,17 +4,52 @@
     using AsyncMethodsInterceptionDemo.Services.Superheros;
     using Autofac;
     using Autofac.AsyncExtras.DynamicProxy;
+    using Castle.DynamicProxy;
 
     public class SuperHerosModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
+            // Registering Interceptors with Castle.Core.AsyncInterceptor in Autofac
             builder
-                .RegisterType<SuperherosService>()
+                .Register(ctx =>
+                {
+                    var superherosService = new SuperherosService();
+
+                    var proxyGenerator = new ProxyGenerator();
+
+                    var transactionHandlingInterceptor =
+                        ctx.Resolve<TransactionInterceptor>();
+
+                    var transactionProxy = proxyGenerator
+                        .CreateInterfaceProxyWithTargetInterface<ISuperherosService>(superherosService, transactionHandlingInterceptor);
+
+                    var exceptionHandlingInterceptor =
+                        ctx.Resolve<ExceptionHandlingInterceptor>();
+
+                    var exceptionHandlingProxy = proxyGenerator
+                        .CreateInterfaceProxyWithTargetInterface(transactionProxy, exceptionHandlingInterceptor);
+
+                    var loggingInterceptor =
+                        ctx.Resolve<LoggingInterceptor>();
+
+                    var loggingProxy = proxyGenerator
+                        .CreateInterfaceProxyWithTargetInterface(exceptionHandlingProxy, loggingInterceptor);
+
+                    return loggingProxy;
+                })
                 .As<ISuperherosService>()
-                .EnableAsyncInterfaceInterceptors()
-                .InterceptedBy(typeof(LoggingInterceptor), typeof(ExceptionHandlingInterceptor), typeof(TransactionInterceptor))
                 .InstancePerLifetimeScope();
+
+
+            // Registering interceptors using custom Autofac extension methods
+            // NB! Latest version of Autofac has breaking changes and this approach needs adjsutments.
+            //builder
+            //    .RegisterType<SuperherosService>()
+            //    .As<ISuperherosService>()
+            //    .EnableAsyncInterfaceInterceptors()
+            //    .InterceptedBy(typeof(LoggingInterceptor), typeof(ExceptionHandlingInterceptor), typeof(TransactionInterceptor))
+            //    .InstancePerLifetimeScope();
         }
     }
 }
